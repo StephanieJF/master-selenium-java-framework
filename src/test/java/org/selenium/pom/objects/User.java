e062e1127b385e75895f7254f18151d5a4cb8ee8
Stephanie Fischler <sjfischler@gmail.com>
2023-08-11 06:43:52 -0400

    Set up api integration

--- src/test/java/org/selenium/pom/objects/User.java
+++ src/test/java/org/selenium/pom/objects/User.java
@@ -5,13 +5,20 @@ public class User {
 	
 	private String username;
 	private String password;
-	
+	private String email;
+
 	public User() {
 	}
 	
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
+	}
+	
+	public User(String username, String password, String email) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
 	}
 	
 	public String getUsername() {
@@ -26,6 +33,15 @@ public class User {
 	}
 	public User setPassword(String password) {
 		this.password = password;
+		return this;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public User setEmail(String email) {
+		this.email = email;
 		return this;
 	}
 

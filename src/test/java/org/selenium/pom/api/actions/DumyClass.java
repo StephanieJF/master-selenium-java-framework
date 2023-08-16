47c5339aea27537190ff45358c51ecb296a48f74
Stephanie Fischler <sjfischler@gmail.com>
2023-08-16 15:55:53 -0400

    Add screenshotting on failure

--- src/test/java/org/selenium/pom/api/actions/DumyClass.java
+++ src/test/java/org/selenium/pom/api/actions/DumyClass.java
@@ -2,10 +2,6 @@ package org.selenium.pom.api.actions;
 
 import java.io.IOException;
 
-import org.selenium.pom.objects.BillingAddress;
-import org.selenium.pom.objects.User;
-import org.selenium.pom.utils.FakerUtils;
-import org.selenium.pom.utils.JacksonUtils;
 
 public class DumyClass {
 	
--- src/test/java/org/selenium/pom/factory/abstractFactory/ChromeDriverManagerAbstract.java
+++ src/test/java/org/selenium/pom/factory/abstractFactory/ChromeDriverManagerAbstract.java
@@ -1,6 +1,5 @@
 package org.selenium.pom.factory.abstractFactory;
 
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
